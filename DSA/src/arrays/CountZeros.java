ab33f2b076a906f68ba269c5c599acce44803435
sanju <kalpana.surishetty@gmail.com>
2025-01-04 15:33:37 +0530

    permutation

--- DSA/src/arrays/CountZeros.java
+++ DSA/src/arrays/CountZeros.java
@@ -1,5 +1,7 @@
 package arrays;
 
 public class CountZeros {
-	
+	public CountZeros() {
+       // super(); // This explicitly calls the Object class constructor (it's optional here, but good practice)
+    }
 }
---
+++ DSA/src/permutation/GeneratePermutation.java
@@ -0,0 +1,7 @@
+package permutation;
+
+public class GeneratePermutation {
+	public GeneratePermutation() {
+		super();
+    }
+}
